2b2b140b657da82fc4d3b682cb2b2527f39ed1a1
mmancini@uoc.edu <mmancini@uoc.edu>
2019-12-21 11:01:52 +0000

    Remove sudo as it is not good practice

--- beats-installer/installer.sh
+++ beats-installer/installer.sh
@@ -17,23 +17,23 @@ else
         apt update ; apt install wget vim -y
 
         wget https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-7.4.0-amd64.deb
-        sudo dpkg -i packetbeat-7.4.0-amd64.deb
+        dpkg -i packetbeat-7.4.0-amd64.deb
 
 
         wget https://artifacts.elastic.co/downloads/beats/auditbeat/auditbeat-7.4.0-amd64.deb
-        sudo dpkg -i auditbeat-7.4.0-amd64.deb
+        dpkg -i auditbeat-7.4.0-amd64.deb
 
 
 
         export OSQUERY_KEY=1484120AC4E9F8A1A577AEEE97A80C63C9D8B80B
-        sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $OSQUERY_KEY
-        sudo add-apt-repository 'deb [arch=amd64] https://pkg.osquery.io/deb deb main'
-        sudo apt-get  update
-        sudo apt-get install osquery -y
+        apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $OSQUERY_KEY
+        add-apt-repository 'deb [arch=amd64] https://pkg.osquery.io/deb deb main'
+        apt-get  update
+        apt-get install osquery -y
 
 
         wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.4.0-amd64.deb
-        sudo dpkg -i filebeat-7.4.0-amd64.deb
+        dpkg -i filebeat-7.4.0-amd64.deb
       fi
 
 
@@ -49,23 +49,22 @@ else
         fi
 
       #Load Kibana
-      sudo sed 's/  #host: "'$arg2':5601"/  host: "'$arg2':5601"/' filebeat_example.yml > temp.txt; sudo cp temp.txt /etc/filebeat/filebeat.yml
-      sudo sed 's/  #host: "'$arg2':5601"/  host: "'$arg2':5601"/' packetbeat_example.yml > temp.txt; sudo cp temp.txt /etc/packetbeat/packetbeat.yml
-      sudo sed 's/  #host: "'$arg2':5601"/  host: "'$arg2':5601"/' auditbeat_example.yml > temp.txt; sudo cp temp.txt /etc/auditbeat/auditbeat.yml
+      sed 's/  #host: "'$arg2':5601"/  host: "'$arg2':5601"/' filebeat_example.yml > temp.txt; cp temp.txt /etc/filebeat/filebeat.yml
+      sed 's/  #host: "'$arg2':5601"/  host: "'$arg2':5601"/' packetbeat_example.yml > temp.txt; cp temp.txt /etc/packetbeat/packetbeat.yml
+      sed 's/  #host: "'$arg2':5601"/  host: "'$arg2':5601"/' auditbeat_example.yml > temp.txt; cp temp.txt /etc/auditbeat/auditbeat.yml
 
       # Loading
-      sudo filebeat setup
-      sudo packetbeat setup --dashboards
-      sudo auditbeat setup --dashboards
+      filebeat setup
+      packetbeat setup --dashboards
+      auditbeat setup --dashboards
 
       fi
 
       if [ $arg1 = "start" ]
       then
-        sudo service filebeat start
-        sudo service packetbeat start
-        sudo service auditbeat start
+        service filebeat start
+        service packetbeat start
+        service auditbeat start
       fi
 
 fi
-
